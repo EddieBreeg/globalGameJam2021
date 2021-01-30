a41baa3a29c1a589afdc37391144a40d0c32eab9
Metamaus <leonard.rizzo71@hotmail.fr>
2021-01-30 11:20:13 +0100

    main hacking functions

--- ggj2021/Assets/Scripts/Hackable.cs
+++ ggj2021/Assets/Scripts/Hackable.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public abstract class Hackable : Interactable
 {
+    public GameObject windowObject;
+
     public bool[] powerups;
     abstract public void hack(PlayerController.Powerups pu, PlayerController controller);
+
+    void Start(){
+        powerups = new bool[Globally.nbPowerups];
+    }
+
+    public override void interact(PlayerController controller){
+        //launch ui
+        windowObject.SetActive(true);
+        controller.uiOn();
+    }   
 
 }
---
+++ ggj2021/Assets/Scripts/HackableWindow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HackableWindow : MonoBehaviour
+{
+    public PlayerController controller;
+    public GameObject mySelf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable(){
+        //isenabled
+    }
+
+    public void turnOff(){
+        controller.uiOff();
+        mySelf.SetActive(false);
+    }
+
+    public void launchHack(int pu){
+        Hackable hobj = (Hackable) controller.getFocus();
+        hobj.hack((PlayerController.Powerups) pu, controller);
+    }
+}
