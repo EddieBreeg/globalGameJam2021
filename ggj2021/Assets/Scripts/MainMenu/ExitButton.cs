2f6a21216db809dbd44d594f5707a39e8434a0ba
Kaïs-Khan Hadi <kaiskhan.hadi@gmail.com>
2021-01-30 18:28:43 +0100

    Main Menu created

---
+++ ggj2021/Assets/Scripts/MainMenu/ExitButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static ChangeScene;
+
+public class ExitButton : MonoBehaviour
+{
+
+    private Button exit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exit = GetComponent<Button>();
+        exit.onClick.AddListener(() => ChangeScene.exit());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ggj2021/Assets/Scripts/MainMenu/LaunchButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static ChangeScene;
+using UnityEngine.UI;
+
+public class LaunchButton : MonoBehaviour
+{
+
+    private Button launch;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        launch = GetComponent<Button>();
+        launch.onClick.AddListener(() => ChangeScene.changeScene("Scenes/Tests"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
