8b2a2cd47a19ea19e4bdbc55fcef964b7e6990b0
Kaïs-Khan Hadi <kaiskhan.hadi@gmail.com>
2021-01-30 01:45:42 +0100

    Added hack

---
+++ ggj2021/Assets/Scripts/Hackable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Hackable : Interactable
+{
+    public bool[] powerups;
+    abstract public void hack(PlayerController.Powerups pu, PlayerController controller);
+
+}
