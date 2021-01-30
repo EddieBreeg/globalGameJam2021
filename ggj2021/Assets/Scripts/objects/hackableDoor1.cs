25ba1d65d66dfdc9729cb9d2f1bd3fe8726e7ca3
Metamaus <leonard.rizzo71@hotmail.fr>
2021-01-30 13:57:45 +0100

    added hackable

---
+++ ggj2021/Assets/Scripts/objects/hackableDoor1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hackableDoor1 : Hackable
+{
+    public AudioClip soundNull;
+    public AudioSource source;
+
+    bool active;
+
+    public override void hack(PlayerController.Powerups pu, PlayerController controller){
+        powerups[(int)pu] = true;
+
+        switch (pu)
+        {
+            case PlayerController.Powerups.Activate:
+                //do something
+                Debug.Log("Activate");
+                active = true;
+                break;
+            case PlayerController.Powerups.Open:
+                //do something
+                if (active){
+                    Debug.Log("Open");
+                }
+                 break;
+            default:
+                //do something
+                break;
+        }
+    }
+}
