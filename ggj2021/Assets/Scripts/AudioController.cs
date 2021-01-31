ce0dcae01362d183e2c16035bf72f8baef67ebee
ThalMug <florian@florian-PC.localdomain>
2021-01-31 16:40:50 +0100

    soundslevel1

---
+++ ggj2021/Assets/Scripts/AudioController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] audioClipArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = audioClipArray[0];
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(! audioSource.isPlaying){
+            audioSource.loop = true;
+            audioSource.clip = audioClipArray[1];
+            audioSource.Play();
+
+        }
+    }
+}
